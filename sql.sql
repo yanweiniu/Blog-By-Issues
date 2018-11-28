9eec49803dc8e9169df6ec9ca076b4e4c05fc8fc
yanwei <2804695074@qq.com>
2018-11-28 13:11:17 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,3 +1,4 @@
+-----------------------------基础查询语法---------------------
 -- change database
 use database_name
 
@@ -131,5 +132,43 @@ SELECT id, order_num FROM orders WHERE Year(order_date) = 2005 AND Month(order_d
 -- Second(expr)           | return second part of expr
 -- DayOfWeek(expr)        | return day of expr
 -- DateDiff(expr1, expr2) | return expr1 - expr2
-………………
 
+-- aggregate function
+-- Avg()
+-- Count()
+-- Max()
+-- Min()
+-- Sum()
+
+------分组查询 GROUP  HAVING
+-- +---------+---------+----------------+------------+----------------------------------------------------------------+
+-- | prod_id | vend_id | prod_name      | prod_price | prod_desc                                                      |
+-- +---------+---------+----------------+------------+----------------------------------------------------------------+
+-- | ANV01   |    1001 | .5 ton anvil   |       5.99 | .5 ton anvil, black, complete with handy hook                  |
+-- | ANV02   |    1001 | 1 ton anvil    |       9.99 | 1 ton anvil, black, complete with handy hook and carrying case |
+-- | ANV03   |    1001 | 2 ton anvil    |      14.99 | 2 ton anvil, black, complete with handy hook and carrying case |
+-- | DTNTR   |    1003 | Detonator      |      13.00 | Detonator (plunger powered), fuses not included                |
+-- | FB      |    1003 | Bird seed      |      10.00 | Large bag (suitable for road runners)                          |
+-- | FC      |    1003 | Carrots        |       2.50 | Carrots (rabbit hunting season only)                           |
+-- | FU1     |    1002 | Fuses          |       3.42 | 1 dozen, extra long                                            |
+-- | JP1000  |    1005 | JetPack 1000   |      35.00 | JetPack 1000, intended for single use                          |
+-- | JP2000  |    1005 | JetPack 2000   |      55.00 | JetPack 2000, multi-use                                        |
+-- | OL1     |    1002 | Oil can        |       8.99 | Oil can, red                                                   |
+-- | SAFE    |    1003 | Safe           |      50.00 | Safe with combination lock                                     |
+-- | SLING   |    1003 | Sling          |       4.49 | Sling, one size fits all                                       |
+-- | TNT1    |    1003 | TNT (1 stick)  |       2.50 | TNT, red, single stick                                         |
+-- | TNT2    |    1003 | TNT (5 sticks) |      10.00 | TNT, red, pack of 10 sticks                                    |
+-- +---------+---------+----------------+------------+----------------------------------------------------------------+
+
+SELECT vend_id, Count(*) AS num_prod
+  FROM products
+  GROUP BY vend_id
+  HAVING Count(*) >= 2;
+-- +---------+----------+
+-- | vend_id | num_prod |
+-- +---------+----------+
+-- |    1001 |        3 |
+-- |    1002 |        2 |
+-- |    1003 |        7 |
+-- |    1005 |        2 |
+-- +---------+----------+
